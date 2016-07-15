55f0da7da5aa66c5073c06d158f853805ee89433
twang <meng-junw@hpe.com>
2016-07-15 18:00:25 +0800

    improve

--- public/javascript/user/userDetail/userDetail.js
+++ public/javascript/user/userDetail/userDetail.js
@@ -2,6 +2,7 @@ agMain.controller("userDetail", function($scope,utility){
 	$scope.data = null;
 	function getUserData(){
 		var userData = $("#myStoreData").text();
+		userData = utility.trimeProfileUrlObject(JSON.parse(userData));         
 		console.log(userData);
 
 
