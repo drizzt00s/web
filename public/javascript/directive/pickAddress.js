6007bb90c63d06b98d2a3e9d4d52c3ed34715230
twang <meng-junw@hpe.com>
2016-07-18 16:56:28 +0800

    improve

--- public/javascript/directive/pickAddress.js
+++ public/javascript/directive/pickAddress.js
@@ -1,7 +1,15 @@
 agMain.directive('pickaddress', function(){
 	return {
 		restrict:"E",
-		templateUrl:'/pickAddress'
+		templateUrl:'/pickAddress',
+		scope:{
+
+		},
+		link:function(scope){
+			scope.popUpAddress = function(){
+				alert(11);
+			}
+		}
 	}
 });
 
--- public/javascript/register/register.js
+++ public/javascript/register/register.js
@@ -2,6 +2,6 @@ agMain.controller('register', function($scope){
 	$scope.defaultYearVal = '请选择年';
 	$scope.defaultMonthVal = '请选择月';
 	$scope.defaultDayVal = '请选择日';
-	
-	window.showCalendar1();
+
+	//window.showCalendar1();
 })
